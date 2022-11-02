a66528f4ad1c95ebcbe8e2c92b340e1ea73231f2
kbeni <72067078+KonyaBenedek@users.noreply.github.com>
2022-11-02 19:35:11 +0100

    Add files via upload
    -Visszaigazolás nem működik 
    -A JavaScript egyenesen rossz, de működik (Nem volt hajlandó getElementsByClassName-mel csinálni)

---
+++ script.js
@@ -0,0 +1,29 @@
+function effektBeNev() {
+    var stuff = document.getElementById("nev");
+    stuff.style.background = "lightskyblue"
+}
+
+function effektBeCim() {
+    var stuff = document.getElementById("cim");
+    stuff.style.background = "lightskyblue"
+}
+
+function effektBeTel() {
+    var stuff = document.getElementById("telszam");
+    stuff.style.background = "lightskyblue"
+}
+
+function effektBlurNev() {
+    var dolog = document.getElementById("nev")
+    dolog.style.background = "white";
+}
+
+function effektBlurCim() {
+    var dolog = document.getElementById("cim")
+    dolog.style.background = "white";
+}
+
+function effektBlurTel() {
+    var dolog = document.getElementById("telszam")
+    dolog.style.background = "white";
+}
